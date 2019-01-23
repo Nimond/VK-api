c05e24ac4ce207e6606b2e5358b6a38096e63efa
Nimond <nimondars@gmail.com>
2019-01-23 15:54:39 +0300

    changing name

--- VpyK/longpoll.py
+++ VpyK/longpoll.py
@@ -2,11 +2,7 @@ import requests
 import json
 import time
         
-
-class methods:
-    pass
-
-
+        
 class longpoll:
     def get_longpoll(self):
         self.logging("Getting longpoll server..")
@@ -54,21 +50,3 @@ class longpoll:
             print(text)
 
 
-class api(methods, longpoll):
-    def __init__(self, token, v, polling = 1, log=1):
-        self.token = token
-        self.log = log
-
-        if v == 'last':
-            from bs4 import BeautifulSoup
-            import requests
-
-            soup = BeautifulSoup(requests.get('https://vk.com/dev/versions').text, 'lxml')
-            v = soup.find('span', {'class': 'dev_version_num'}).text
-            self.logging(v)
-
-        self.v = v
-        
-
-        if polling:
-            longpoll.get_longpoll(self)
