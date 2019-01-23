11d3c024a454d969fc01484be35b8de2fe43a468
Nimond <nimondars@gmail.com>
2019-01-23 15:19:11 +0300

    no version support

--- longpoll.py
+++ longpoll.py
@@ -57,8 +57,18 @@ class longpoll:
 class api(methods, longpoll):
     def __init__(self, token, v, polling = 1, log=1):
         self.token = token
-        self.v = v
         self.log = log
+
+        if v == 'last':
+            from bs4 import BeautifulSoup
+            import requests
+
+            soup = BeautifulSoup(requests.get('https://vk.com/dev/versions').text, 'lxml')
+            v = soup.find('span', {'class': 'dev_version_num'}).text
+            self.logging(v)
+
+        self.v = v
+        
 
         if polling:
             longpoll.get_longpoll(self)
